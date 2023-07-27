0181689ffa5251b041e038e25de3bfa74e27ffa4
maodzhedun <maodzhedun@gmail.com>
2023-07-28 00:34:21 +0300

    third start

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,20 @@
+import throttle from "lodash.throttle";
+// console.log(localStorage);
+
+
+const form = document.querySelector(".feedback-form");
+// console.log(form);
+const email = document.querySelector(".feedback-form input");
+// console.log(email);
+const msg = document.querySelector(".feedback-form textarea")
+// console.log(msg);
+
+
+
+form.addEventListener('input', inputData);
+
+function inputData(event) {
+    console.log(event.currentTarget.value);
+    localStorage.setItem("feedback-form-state", JSON.stringify({email, msg}));
+}
 
