6afb65d323e3002df2f61c53d28475177a19f2a0
maodzhedun <48294837+maodzhedun@users.noreply.github.com>
2023-07-28 15:59:38 +0300

    Third task end

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,20 +1,35 @@
 import throttle from "lodash.throttle";
-// console.log(localStorage);
 
+const DATA_LS_KEY = "feedback-form-state";
 
 const form = document.querySelector(".feedback-form");
-// console.log(form);
-const email = document.querySelector(".feedback-form input");
-// console.log(email);
-const msg = document.querySelector(".feedback-form textarea")
-// console.log(msg);
 
+form.addEventListener('input', throttle(inputData, 500));
+form.addEventListener('submit', formSubmit);
+let objectUserDAta = {};
 
-
-form.addEventListener('input', inputData);
+loadPage();
 
 function inputData(event) {
-    console.log(event.currentTarget.value);
-    localStorage.setItem("feedback-form-state", JSON.stringify({email, msg}));
+    objectUserDAta[event.target.name] = event.target.value.trim();
+    console.log(objectUserDAta);
+    localStorage.setItem(DATA_LS_KEY, JSON.stringify(objectUserDAta));
+ }
+
+function loadPage() { 
+    const data = localStorage.getItem(DATA_LS_KEY);
+    if (!data) {
+        return;
+    } 
+    objectUserDAta = JSON.parse(data);
+    Object.entries(objectUserDAta).forEach(([key, val]) => {
+        form.elements[key].value = val
+    })
 }
 
+function formSubmit(event) {
+    event.preventDefault();
+    console.log(objectUserDAta);
+    event.currentTarget.reset();
+    localStorage.removeItem(DATA_LS_KEY);
+}
